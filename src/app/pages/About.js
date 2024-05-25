5deaf9e71977d8cb3cbc8838054e2d3612a26471
1mshy <imshi1@protonmail.com>
2024-05-24 20:57:49 -0400

    got rid of all daniel's work

---
+++ src/app/pages/About.js
@@ -0,0 +1,16 @@
+"use client"
+
+import React, {Component} from 'react';
+
+export default class About extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                fsdfsdf
+            </div>
+        )
+    }
+}
