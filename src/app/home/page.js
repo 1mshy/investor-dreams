a803508a294066a5460dfb327b81e62b04ae3ca0
1mshy <imshi1@protonmail.com>
2024-05-25 14:31:41 -0400

    Changed the icons
    Changed the icons Changed the icons Changed the icons Changed the icons Changed the icons Changed the icons Changed the icons Changed the icons Changed the icons

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,38 +1,10 @@
-"use client"
-import React, { Component } from 'react';
-import Link from 'next/link';
-import "../css/Homepage.css"
-import { invoke } from '@tauri-apps/api';
+import dynamic from "next/dynamic";
+import React from "react";
 
-export default class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: ""
-        }
-    }
+const HomeNoSSR = dynamic(() => import("../ui_components/pages/Home"), {
+    ssr: false
+});
 
-    async componentDidMount() {
-        invoke("get_username").then((response) => {
-            this.setState({ username: response });
-        });
-    }
-
-    render() {
-        const { username } = this.state;
-        return (
-            <div className={"mainPage"}>
-                <header className={"header"}>
-                    <h1 className={"title"}>Investor Dreams</h1>
-                    <nav className={"nav"}>
-                        <Link href="/playground" className={"navButton"}>Top 10</Link>
-                        <Link href="/" className={"navButton"}>Top 300</Link>
-                    </nav>
-                </header>
-                <div className={"homepage-content"}>
-                    <h2 className={"welcomeText"}>Welcome back {username}</h2>
-                </div>
-            </div>
-        );
-    }
+export default function HomePage() {
+    return <HomeNoSSR />;
 }
