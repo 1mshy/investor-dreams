a1cb9c9e7104ffd6ad79a20f1961afe79bbcca53
1mshy <imshi1@protonmail.com>
2024-05-24 21:45:07 -0400

    added homepage

---
+++ src/app/main/page.js
@@ -0,0 +1,27 @@
+"use client"
+import React, { Component } from 'react';
+import Link from 'next/link';
+import "../css/Homepage.css"
+
+export default class Home extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className={"mainPage"}>
+                <header className={"header"}>
+                    <h1 className={"title"}>Investor Dreams</h1>
+                    <nav className={"nav"}>
+                        <Link href="/home" className={"navButton"}>Top 10</Link>
+                        <Link href="/" className={"navButton"}>Top 300</Link>
+                    </nav>
+                </header>
+                <div className={"content"}>
+                    <h2 className={"welcomeText"}>HI THERE PERSON</h2>
+                </div>
+            </div>
+        );
+    }
+}
