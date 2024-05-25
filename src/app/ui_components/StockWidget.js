2760dfb4f2fda80fb3d67d8459d7832307bf8299
1mshy <imshi1@protonmail.com>
2024-05-24 20:41:02 -0400

    very shit implementation of a big graph, will work on after finishing the routing

--- src/app/ui_components/StockWidget.js
+++ src/app/ui_components/StockWidget.js
@@ -1,8 +1,10 @@
 "use client";
 
 import styled from 'styled-components';
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import PriceGraph from "@/app/ui_components/PriceGraph";
+import BigStockWidget from './BigStockWidget';
+import SmallStockWidget from './SmallStockWidget';
 
 
 /**
@@ -10,39 +12,21 @@ import PriceGraph from "@/app/ui_components/PriceGraph";
  * @type {IStyledComponent<"web", Substitute<import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLDivElement>, HTMLDivElement>, BaseObject>> & BaseObject & {}}
  * @desc affects colour if positive or negative (green or red)
  */
-const StockChange = (props) => {
-    return <div style={{color: props.isPositive ? '#4caf50' : '#e74c3c'}}>
+export const StockChange = (props) => {
+    return <div style={{ color: props.isPositive ? '#4caf50' : '#e74c3c' }}>
         {...props.children}
     </div>
 }
 
 
-const StockWidget = ({symbol, name, exchange, price, change, date, historical_prices}) => {
-    const [isPositive, setIsPositive] = useState(change >= 0);
-
-    // Optionally, use an effect to update isPositive when the change prop updates
-    useEffect(() => {
-        setIsPositive(change >= 0);
-    }, [change]);
+export const StockWidget = ({ symbol, name, exchange, price, change, date, historical_prices }) => {
+    const [big, setBig] = useState(false);
     return (
-        <div className={"container"}>
-            <div className={"head"}>
-                <div className={"ticker_symbol"}>{symbol}</div>
-                <div className={"company_name"}>{name} ({exchange})</div>
-            </div>
-            <div className={"content"}>
-                <div className={"price"}>${price}</div>
-                <PriceGraph prices={historical_prices}/>
-                <div className={"price-data"}>
-                    <div className={"price-change"}>
-                        <StockChange isPositive={isPositive}>{isPositive ? '+' : ''}{change}%</StockChange>
-                    </div>
-                    <div className={"date"}>
-                        {date}
-                    </div>
-                </div>
-            </div>
-        </div>
+        <>
+            {big ?
+                <BigStockWidget symbol={symbol} name={name} exchange={exchange} price={price} change={change} date={date} historical_prices={historical_prices} onClick={()=>{setBig(false)}}/> :
+                <SmallStockWidget symbol={symbol} name={name} exchange={exchange} price={price} change={change} date={date} historical_prices={historical_prices} onClick={()=>{setBig(true)}}/>
+            }
+        </>
     );
 };
-export default StockWidget;
