ceaf54a9d30d8f56d86a3568ca0ce70952971bfc
1mshy <imshi1@protonmail.com>
2024-05-25 14:31:41 -0400

    Changed the icons
    Changed the icons Changed the icons Changed the icons Changed the icons Changed the icons Changed the icons Changed the icons Changed the icons Changed the icons

--- src/app/playground/page.js
+++ src/app/playground/page.js
@@ -1,10 +1,10 @@
 // pages/index.js or pages/playground.js
 import dynamic from 'next/dynamic';
 
-const Home = dynamic(() => import('../ui_components/containers/Playground'), {
+const Home = dynamic(() => import('../ui_components/pages/Playground'), {
     ssr: false
 });
 
-export default function HomePage() {
+export default function PlaygroundPage() {
     return <Home />;
 }
---
+++ src/app/ui_components/pages/Home.js
@@ -0,0 +1,41 @@
+"use client"
+import React, { Component } from 'react';
+import Link from 'next/link';
+import "../../css/Homepage.css";
+import { invoke } from '@tauri-apps/api';
+
+export default class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: ""
+        }
+    }
+
+    async componentDidMount() {
+        // Only access browser-specific APIs here
+        invoke("get_username").then((response) => {
+            this.setState({ username: response });
+        }).catch(error => {
+            console.error("Failed to fetch username:", error);
+        });
+    }
+
+    render() {
+        const { username } = this.state;
+        return (
+            <div className={"mainPage"}>
+                <header className={"header"}>
+                    <h1 className={"title"}>Investor Dreams</h1>
+                    <nav className={"nav"}>
+                        <Link href="/playground" className={"navButton"}>Top 10</Link>
+                        <Link href="/" className={"navButton"}>Top 300</Link>
+                    </nav>
+                </header>
+                <div className={"homepage-content"}>
+                    <h2 className={"welcomeText"}>Welcome back {username}</h2>
+                </div>
+            </div>
+        );
+    }
+}
