a0a797e5a1b7cf896047015b3d1ecc5e586e3d95
1mshy <imshi1@protonmail.com>
2024-05-26 17:34:14 -0400

    no need for client side mapping for the ticker to company name

--- src/app/funcs/scraper.js
+++ src/app/funcs/scraper.js
@@ -1,12 +1,17 @@
 
 import { invoke } from '@tauri-apps/api';
-import cheerio from 'cheerio';
+import cheerio, { load } from 'cheerio';
 
-export async function request_top_companies() {
-    console.log("runnins top comps")
+const local_storage_key = "s&p500_ticker_name_percent";
+
+/**
+ * @description Get the S&P 500 list of companies with their ticker symbol, company name and portfolio percentage
+ * @returns {Promise<Object<string, {number: number, company: string, portfolio_percent: string}>}
+ */
+async function request_top_companies() {
     const text = await invoke('get_index_info');
-    const $ = cheerio.load(text);
-    const data = [];
+    const $ = load(text);
+    const data = {};
     $('table.table tbody tr').each((index, element) => {
         const cells = $(element).find('td');
         if (cells.length >= 4) {
@@ -14,9 +19,28 @@ export async function request_top_companies() {
             const company = $(cells[1]).text().trim();
             const ticker_symbol = $(cells[2]).text().trim();
             const portfolio_percent = $(cells[3]).text().trim();
-            data.push({ number, company, ticker_symbol, portfolio_percent });
+            data[ticker_symbol] = { number, company, portfolio_percent };
         }
     });
-
     return data;
+}
+/**
+ * @desc exposed caching function for the requesting of the S&P 500 list
+ * @returns {Promise<Object<string, {number: number, company: string, portfolio_percent: string}>}
+ */
+export async function get_sp_500_data() {
+    let data = localStorage.getItem(local_storage_key);
+    if (!data) {
+        data = await request_top_companies();
+        localStorage.setItem(local_storage_key, JSON.stringify(data));
+    } else {
+        data = JSON.parse(data);
+    }
+    return data;
+}
+
+
+export async function ticker_to_name(ticker_symbol) {
+    const data = await get_sp_500_data();
+    return data[ticker_symbol].company;
 }
