a559828fe6c10dcf78b391c5fd0a7c3a5f15ffa3
imshy <imshi1@protonmail.com>
2024-10-18 00:13:33 -0400

    added testinggit status

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import BasePage from "./app/app";
 
 import "@/app/css/globals.css";
-import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById("application")).render(
