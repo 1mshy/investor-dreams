6f0925629cbe1c9ca9989ce900dcf6e9a2f22b04
imshy <imshi1@protonmail.com>
2024-11-27 15:13:55 -0500

    set app to two thirds of the screen size

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,13 +5,13 @@ use std::env;
 
 use crate::ollama::ollama_generate;
 use crate::requesting::{
-    get_all_static_ticker_info, get_request_api, req_nasdaq_info, request_deep, reddit_request_api
+    get_all_static_ticker_info, get_request_api, reddit_request_api, req_nasdaq_info, request_deep,
 };
 use crate::sensitive_data::{
     get_all_windows, get_api_keys, get_current_monitor_info, get_username, set_base_size,
 };
-use crate::tools::{save_json_file, save_json_to_folder, close_window, is_macos};
-use tauri::{Manager, Url};
+use crate::tools::{close_window, is_macos, save_json_file, save_json_to_folder};
+use tauri::{LogicalSize, Manager, Size, Url};
 use window_vibrancy::{apply_acrylic, apply_vibrancy, NSVisualEffectMaterial, NSVisualEffectState};
 mod ollama;
 mod requesting;
@@ -33,6 +33,29 @@ pub fn run() {
         .manage(ollama_instance) // Manage the Ollama instance
         .setup(|app| {
             let window = app.get_webview_window("main").unwrap();
+            // setting screen size to 2/3 of the screen
+            match window.current_monitor() {
+                Ok(monitor) => {
+                    let current_monitor = monitor.unwrap();
+                    let scale_factor = current_monitor.scale_factor();
+                    let two_thirds_screen = (2f64 / 3f64) / scale_factor;
+                    let width =
+                        current_monitor.size().width as f64 * two_thirds_screen;
+                    let height =
+                        current_monitor.size().height as f64 * two_thirds_screen;
+                    match window.set_size(Size::Logical(LogicalSize { width, height })) {
+                        Ok(_) => {
+                            println!("Set window size to: {}x{}", width, height);
+                        }
+                        Err(e) => {
+                            println!("Error setting window size: {:?}", e);
+                        }
+                    }
+                }
+                Err(e) => {
+                    println!("Error getting monitor: {:?}", e);
+                }
+            }
             // second window
             // let webview_url = tauri::WebviewUrl::External(Url::parse("https://google.com").unwrap());
             // tauri::WebviewWindowBuilder::new(app, "second", webview_url)
