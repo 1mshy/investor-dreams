26d6c5f382bcbc226ef7d818d6c97511bdb73ccc
imshy <imshi1@protonmail.com>
2024-12-07 16:29:45 -0500

    fixed headers

--- src-tauri/src/requesting.rs
+++ src-tauri/src/requesting.rs
@@ -10,8 +10,8 @@ async fn get_request(url: &str) -> Result<String, Box<dyn Error>> {
     let response = client
         .get(url)
         // Set headers to mimic a browser request
-        .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36")
-        .header("Accept", "*/*")
+        .header("User-Agent", "PostmanRuntime/7.39.0")
+        .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
         .header("Connection", "keep-alive")
         .send()
         .await?;
