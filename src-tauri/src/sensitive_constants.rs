aa8011f57750f3e27ac6ab7b79717e31c3fe5405
imshy <imshi1@protonmail.com>
2024-12-29 20:23:14 -0500

    constants

--- src-tauri/src/sensitive_constants.rs
+++ src-tauri/src/sensitive_constants.rs
@@ -2,6 +2,10 @@
 // seperate them in the string with commas (same as env file)
 pub const BUILD_API_KEYS:&str = "";
 
-// reddit api username, and password
-pub const REDDIT_API_USERNAME:&str = "";
-pub const REDDIT_API_PASSWORD:&str = "";
+/**
+ * Reddit api information. Sign up for reddit api and put in the info
+ */
+pub const CLIENT_ID: &str = ""; 
+pub const CLIENT_SECRET: &str = "";
+pub const REDDIT_API_USERNAME: &str = "";
+pub const REDDIT_API_PASSWORD: &str = "";
